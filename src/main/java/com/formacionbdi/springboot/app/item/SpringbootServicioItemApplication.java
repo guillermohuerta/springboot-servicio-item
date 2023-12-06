ccbf4fcff49d1b58eb3988f6e1427721a8905bbf
Guillermo Huerta <guillermohuertarondan@gmail.com>
2023-12-05 21:01:31 -0500

    Seccion 5 - 43. Configurando microservicio items con Resilience4J

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,12 +2,10 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableCircuitBreaker
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
