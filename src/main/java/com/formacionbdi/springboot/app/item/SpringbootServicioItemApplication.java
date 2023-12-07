d0bac8fcd2804191f1793cb19b0980267eb0e3c5
Guillermo Huerta <guillermohuertarondan@gmail.com>
2023-12-06 20:06:51 -0500

    Seccion 5 - 45. Probando Resilience con los criterios por defecto

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,10 +2,10 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
