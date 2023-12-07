f2a30c55be8c41a71015eb7855082091319aa14f
Guillermo Huerta <guillermohuertarondan@gmail.com>
2023-12-06 20:21:14 -0500

    Seccion 5 - 46. Perzonalizando parámetro del Circuit Breaker

--- src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
+++ src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
@@ -1,9 +1,17 @@
 package com.formacionbdi.springboot.app.item;
 
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @Configuration
 public class AppConfig {
@@ -13,4 +21,19 @@ public class AppConfig {
     RestTemplate registrarRestTemplate() {
 		return new RestTemplate();
 	}
+    
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizerGuillermo(){
+    	return factory -> factory.configureDefault(id -> {
+    		return new Resilience4JConfigBuilder(id)
+    				.circuitBreakerConfig(CircuitBreakerConfig.custom()
+    						.slidingWindowSize(10)
+    						.failureRateThreshold(50)
+    						.waitDurationInOpenState(Duration.ofSeconds(10L))
+    						.permittedNumberOfCallsInHalfOpenState(5)
+    						.build())
+    				.timeLimiterConfig(TimeLimiterConfig.ofDefaults())
+    				.build();
+    	}); 
+    }
 }
