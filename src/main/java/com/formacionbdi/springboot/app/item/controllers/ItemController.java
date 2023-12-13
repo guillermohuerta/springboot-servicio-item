b2acc75e9a8bde173fa4ff5ac34185ff36471288
Guillermo Huerta <guillermohuertarondan@gmail.com>
2023-12-12 20:23:55 -0500

    Seccion 5 - 52. La anotación @TimeLimiter

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,6 +1,7 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +19,7 @@ import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @RestController
 public class ItemController {
@@ -50,6 +52,13 @@ public class ItemController {
 		return itemService.findById(id, cantidad);
 	}
 	
+	@CircuitBreaker(name = "items_guillermo", fallbackMethod = "metodoAlternativo2")
+	@TimeLimiter(name = "items_guillermo", fallbackMethod = "metodoAlternativo2")//Con esta anotacion solo se controla el timeout, no se hace los corto circuitos, ni tiempo de espera
+	@GetMapping("/ver3/{id}/cantidad/{cantidad}")
+	public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad){
+		return CompletableFuture.supplyAsync(() -> itemService.findById(id, cantidad));
+	}
+	
 	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e){
 		logger.info(e.getMessage());
 		
@@ -63,5 +72,20 @@ public class ItemController {
 		item.setProducto(producto);
 		
 		return item;
+	}
+	
+	public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e){
+		logger.info(e.getMessage());
+		
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		
+		return CompletableFuture.supplyAsync(() -> item);
 	}
 }
